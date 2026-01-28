a7875a6e5f85a3aa3ca56308f9ed05e74b05040c
Christopher Hofmeister <chofmeister@oneeventtech.com>
2026-01-28 07:55:19 -0600

    feat: Introduce standard JSON type string constants

---
+++ Models/JsonTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace ProtocolWorkbench.Core.Models
+{
+    public static class JsonTypes
+    {
+        public const string STRING = "string";
+        public const string INTEGER = "integer";
+        public const string NUMBER = "number";
+        public const string BOOLEAN = "boolean";
+        public const string ARRAY = "array";
+        public const string OBJECT = "object";
+    }
+}
