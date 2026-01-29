7fa84c9d68a0f56bc3675a2559ffef389b343aa2
Christopher Hofmeister <chofmeister@oneeventtech.com>
2026-01-28 21:03:50 -0600

    feat: Introduce AccessType constants

---
+++ Models/AccessType.cs
@@ -0,0 +1,9 @@
+﻿namespace ProtocolWorkbench.Core.Models
+{
+    public class AccessType
+    {
+        public const string ReadWrite = "readwrite";
+        public const string ReadOnly = "readonly";
+        public const string WriteOnly = "writeonly";
+    }
+}
