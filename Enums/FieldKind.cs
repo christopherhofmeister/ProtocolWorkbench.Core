e94943052cda825fedea8d3929e1052a6f383efe
Christopher Hofmeister <chofmeister@oneeventtech.com>
2026-02-04 16:51:32 -0600

    feat: Add general-purpose FieldKind and Flags enums

---
+++ Enums/FieldKind.cs
@@ -0,0 +1,14 @@
+﻿namespace ProtocolWorkbench.Core.Enums
+{
+    public enum FieldKind
+    {
+        String,
+        Integer,
+        Number,
+        Boolean,
+        Object,     // JSON object
+        Array,      // JSON array
+        OneOf,      // union / polymorphic
+        JsonEditor  // Raw JSON editor (object, array, unknown, complex)
+    }
+}
---
+++ Enums/Flags.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtocolWorkbench.Core.Enums
+{
+    public enum Flags
+    {
+        Request = 0,
+        Response = 1,
+        Notification = 2,
+        Error = 3,
+    }
+}
