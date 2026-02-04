9b97d560a90818c9968eaada58c6d63912228004
Christopher Hofmeister <chofmeister@oneeventtech.com>
2026-02-04 16:51:35 -0600

    feat: Introduce binary protocol specific helpers

---
+++ Protocols/Binary/Helpers/BinaryFlags.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtocolWorkbench.Core.Protocols.Binary.Helpers
+{
+    public static class BinaryFlags
+    {
+        public const byte IsResponse = 0b0000_0001;
+        public const byte IsNotification = 0b0000_0010;
+        public const byte IsError = 0b0000_0100;
+        public const byte AckRequested = 0b0000_1000;
+    }
+}
---
+++ Protocols/Binary/Helpers/BinaryType.cs
@@ -0,0 +1,15 @@
+﻿namespace ProtocolWorkbench.Core.Protocols.Binary.Helpers
+{
+    public static class BinaryType
+    {
+        public static ushort Pack(byte category, ushort messageId)
+        {
+            if (category > 0x0F) throw new ArgumentOutOfRangeException(nameof(category));
+            if (messageId > 0x0FFF) throw new ArgumentOutOfRangeException(nameof(messageId));
+            return (ushort)((category << 12) | (messageId & 0x0FFF));
+        }
+
+        public static byte Category(ushort type) => (byte)((type >> 12) & 0x0F);
+        public static ushort MessageId(ushort type) => (ushort)(type & 0x0FFF);
+    }
+}
