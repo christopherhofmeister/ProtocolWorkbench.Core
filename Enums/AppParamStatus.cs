8550a7d69b868df8be0d8dd8d1ae4496b1642246
Christopher Hofmeister <chofmeister@oneeventtech.com>
2026-02-06 17:12:30 -0600

    feat: Introduce AppParamStatus and RpcStatus enums

---
+++ Enums/AppParamStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace ProtocolWorkbench.Core.Enums
+{
+    public enum AppParamStatus : byte
+    {
+        Ok = 0,
+        InvalidArg = 1,
+        NotFound = 2,
+        ReadOnly = 3,
+        WriteOnly = 4,
+        RangeError = 5,
+        InternalError = 6,
+    }
+}
---
+++ Enums/RpcStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace ProtocolWorkbench.Core.Enums
+{
+    public enum RpcStatus : byte
+    {
+        Ok = 0,
+        // Fill these with your real meanings if you already have them:
+        InvalidArg = 1,
+        NotFound = 2,
+        NotAllowed = 3,
+        Busy = 4,
+        Timeout = 5,
+        InternalError = 6,
+        Unknown = 7,
+    }
+}
